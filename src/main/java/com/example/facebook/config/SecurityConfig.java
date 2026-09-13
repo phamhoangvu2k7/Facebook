2dda7ebe8d3e4f0e3b7b0689f50b4517c46e0753
Pham Hoang Vu <phamvuhoang486@gmail.com>
2026-09-13 16:16:46 +0700

    fix: csrf bug

--- src/main/java/com/example/facebook/config/SecurityConfig.java
+++ src/main/java/com/example/facebook/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.example.facebook.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,4 +14,16 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Tắt CSRF
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/**").permitAll() // Mở cửa công khai API Auth
+                        .anyRequest().authenticated() // Các API khác phải có Token
+                );
+        return http.build();
+    }
+
 }
